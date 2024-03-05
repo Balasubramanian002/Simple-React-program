d6f106beaf868acbb8805dc05dce9bbd30eb91d9
Balasubramanian_020 <141569790+Balasubramanian002@users.noreply.github.com>
2024-03-05 22:48:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Propfile from './component/Propfile';
+function App() {
+  return (
+    <div className="App">
+        <Propfile name="raj" age="25"/>
+        <Propfile name="kumar" age="27"/>
+        <Propfile name="gokul" age="28"/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Propfile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export default function Propfile(Propfile){
+     return(<div>
+          <h1>Hello{Propfile.name} with age {Propfile.age}</h1>
+     </div>)
+}
+
